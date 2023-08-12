a71ba40467c885e3985fd8c3738df7928abdf9ca
Avinash <Avinashmate995@gmail.com>
2023-08-12 15:57:51 +0530

    polymorfism solved

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-
+   public static void main(String[] args){
+    Product p = new Product();
+    p.product(2, 5);
+    p.product(3, 4, 8);
+    p.product(5.5, 7.5);
+   }
 }
